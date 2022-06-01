c86d1fea30890616a1a52e77664a2632408b51d7
GM1WKR <gm1wkr@gmail.com>
2022-06-01 18:46:40 +0100

    added index route

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,18 @@ from flask import render_template
 from app import app
 from models.bean_list import *
 
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="MBS")
+
+
 @app.route("/beans")
-def beans():
-    return render_template("beans.html", title="Mean Bean Shop", beans=beans_list)
+def bean_shop():
+    return render_template("beans.html", title="MBS", beans=beans_list)
+
+
+@app.route("/bean/<bean_id>")
+def single_bean(bean_id):
+    chosen_bean = beans_list[int(bean_id)]
+    return render_template("bean.html", title="MBS", bean=chosen_bean)
--- models/bean_list.py
+++ models/bean_list.py
@@ -8,6 +8,6 @@ bean_4 = Bean("Blackeye Bean", "A fiesty little bean who is not afraid to throw 
 bean_5 = Bean("Fava Bean", "The Fava bean is a true favourite for all whose favourite bean is the Fava bean", "12/12/2022", 10.00, False, True)
 bean_6 = Bean("Python Bean", "This bean has bite!  Handle with care and never forget to indent or you might get bitten.", "12/12/2022", 10.00, True, True)
 bean_7 = Bean("Snake Bean", "Really, you expect me to come up with another snake theme for a bean, Hmm, that rhyme will have to do.", "12/12/2022", 10.00, False, False)
-bean_8 = Bean("Justa Bean", "A best selling boring bean, we can't stand these at the Mean Bean so we are selling them to you our valued customers", "12/12/2022", 10.00, False, True)
+bean_8 = Bean("Justa Bean", "Our best selling boring bean, we can't stand these at the Mean Bean so we are selling them to you our valued customers", "12/12/2022", 10.00, False, True)
 
 beans_list = [bean_1, bean_2, bean_3, bean_4, bean_5, bean_6, bean_7, bean_8]
